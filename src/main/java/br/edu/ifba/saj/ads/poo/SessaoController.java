73cffd4c218333149fc94ef666ca8f12d805c8f7
leandro-costa <leandro.costa@ifba.edu.br>
2026-06-02 20:40:32 -0300

    update seleção de filme e localdatetime

--- src/main/java/br/edu/ifba/saj/ads/poo/SessaoController.java
+++ src/main/java/br/edu/ifba/saj/ads/poo/SessaoController.java
@@ -1,23 +1,33 @@
 package br.edu.ifba.saj.ads.poo;
 
 import java.io.IOException;
-import java.util.Objects;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import br.edu.ifba.saj.ads.poo.data.Cinema;
 import br.edu.ifba.saj.ads.poo.model.Filme;
-import javafx.util.StringConverter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.util.StringConverter;
 
 public class SessaoController {
 
     @FXML
-    private DatePicker dtHorario;
+    private DatePicker dtHorarioDia;
+    @FXML
+    private Spinner<Integer> dtHorarioHora;
+    @FXML
+    private Spinner<Integer> dtHorarioMinuto;
 
     @FXML
     private ChoiceBox<Filme> slFilme;
+    private Filme filmeSelecionado;
 
     @FXML
     public void abrirCadastarFilme(ActionEvent event) {
@@ -32,52 +42,51 @@ public class SessaoController {
 
     @FXML
     private void initialize() {
-        slFilme.setConverter(new StringConverter<Filme>() {
-            @Override
-            public String toString(Filme obj) {
-                if (obj != null) {
-                    return obj.getNome() + ":" + obj.getDuracao();
-                }
-                return "";
-            }
-
-            @Override
-            public Filme fromString(String stringFilme) {
-                return Cinema.filmes
-                        .stream()
-                        .filter(filme -> stringFilme.equals(filme.getNome() + ":" + filme.getDuracao()))
-                        .findAny()
-                        .orElse(null);
+        slFilme.getItems().addAll(Cinema.filmes);
+        //quando um filme for selecionado na lista, vai guardar esse filme na variaval "filmeSelecionado"
+        slFilme.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                filmeSelecionado = newValue;                
             }
         });
+        //Define qual a informação do filme vai ser exibida na lista
+        slFilme.setConverter(new StringConverter<Filme>() {
+            @Override
+            public String toString(Filme filme) {
+                // Define o texto que o usuário vai ver na tela
+                return filme == null ? "" : filme.getNome();
+            }
 
-        carregarListaFilmes();
+            @Override
+            public Filme fromString(String string) {
+                // Não é necessário implementar para ChoiceBox padrão (pode retornar null)
+                return null;
+            }
+        });
+    }
+
+    private LocalDateTime getHorarioSelecionado() {
+        LocalDate date = dtHorarioDia.getValue();
+        if (date == null) {
+            return null; 
+        }
+
+        int hour = dtHorarioHora.getValue();
+        int minute = dtHorarioMinuto.getValue();
+        
+        return LocalDateTime.of(date, LocalTime.of(hour, minute));
     }
 
     @FXML
     void salvar(ActionEvent event) {
-        // if ((Objects.nonNull(txDuracao.getText())
-        // && !txDuracao.getText().isEmpty()
-        // && !txDuracao.getText().isEmpty())
-        // &&
-        // (Objects.nonNull(txNome.getText())
-        // && !txNome.getText().isEmpty()
-        // && !txNome.getText().isEmpty())) {
-        // Filme novoFilme = new Filme(txNome.getText(),
-        // Float.valueOf(txDuracao.getText()));
-        // Cinema.filmes.add(novoFilme);
-        // new Alert(AlertType.INFORMATION, String.format("Novo filme %s cadastrado",
-        // novoFilme.getNome())).showAndWait();
-        //
-        // }else{
-        // new Alert(AlertType.ERROR,"Nome e duração são informações
-        // obrigatórias").showAndWait();
-        // }
+
+        new Alert(AlertType.INFORMATION,String.format("Horario filme %1$td/%1$tm/%1$tY %1$tH:%1$tM ",getHorarioSelecionado())).showAndWait();
+        new Alert(AlertType.INFORMATION,String.format("Nome filme %s ",getFilmeSelecionado().getNome())).showAndWait();
 
     }
 
-    private void carregarListaFilmes() {
-        slFilme.setItems(Cinema.filmes);
+    public Filme getFilmeSelecionado() {
+        return filmeSelecionado;
     }
 
 }
--- src/main/java/br/edu/ifba/saj/ads/poo/data/Cinema.java
+++ src/main/java/br/edu/ifba/saj/ads/poo/data/Cinema.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.edu.ifba.saj.ads.poo.model.Filme;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 
 public class Cinema {
-    public static ObservableList<Filme> filmes = FXCollections.observableArrayList();
+    public static List<Filme> filmes = new ArrayList<>();
     
 }
